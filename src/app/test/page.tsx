a00c188a2695a2dec744247c1dc72ac09fc82b10
Tejas Kalpathi <tejaskalpathi@gmail.com>
2024-10-11 20:08:01 -0400

    test page

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TestPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Test Page</h1>
+      <p>This is a template for a new page.</p>
+    </div>
+  );
+};
+
+export default TestPage;
